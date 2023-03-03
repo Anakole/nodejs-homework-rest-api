c09e7b0efd98d91bec6dd101461eb49b470d3dd4
Anakole <104356239+Anakole@users.noreply.github.com>
2023-03-03 13:06:54 +0200

    Create index.js

---
+++ controllers/index.js
@@ -0,0 +1,17 @@
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  updateContact,
+  updateStatusContact,
+  removeContact,
+} = require("./contacts");
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  updateContact,
+  updateStatusContact,
+  removeContact,
+};
