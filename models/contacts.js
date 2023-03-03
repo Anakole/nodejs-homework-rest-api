9edf93150ddb50b9dba07cdf4ef8800453eb918b
Anakole <104356239+Anakole@users.noreply.github.com>
2023-03-03 13:06:56 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -3,39 +3,42 @@ const Joi = require("joi");
 const { handleMongooseError } = require("../helpers");
 
 const contactSchema = new Schema({
-    name: {
-        type: String,
-        required: [true, "Set name for contact"],
-    },
-    email: {
-        type: String,
-    },
-    phone: {
-        type: String,
-    },
-    favorite: {
-        type: Boolean,
-        default: false,
-    },
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 contactSchema.post("save", handleMongooseError);
 
 const addSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    phone: Joi.string().required(),
-    favorite: Joi.boolean(),
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
 });
 
 const updateFavoriteSchema = Joi.object({
-    favorite: Joi.boolean().required(),
+  favorite: Joi.boolean().required(),
 });
 
 const Contact = model("contact", contactSchema);
 
+const schemas = {
+  addSchema,
+  updateFavoriteSchema,
+};
 module.exports = {
-    Contact,
-    addSchema,
-    updateFavoriteSchema,
+  Contact,
+  schemas,
 };
