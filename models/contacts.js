96913b6202af8be26d6da7451ed6d50e4c8297d7
Anakole <104356239+Anakole@users.noreply.github.com>
2023-03-22 23:19:30 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -2,22 +2,34 @@ const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 const { handleMongooseError } = require("../helpers");
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    phone: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 contactSchema.post("save", handleMongooseError);
 
@@ -26,6 +38,7 @@ const addSchema = Joi.object({
   email: Joi.string().email().required(),
   phone: Joi.string().required(),
   favorite: Joi.boolean(),
+  owner: Joi.string().required(),
 });
 
 const updateFavoriteSchema = Joi.object({
@@ -34,11 +47,11 @@ const updateFavoriteSchema = Joi.object({
 
 const Contact = model("contact", contactSchema);
 
-const schemas = {
+const contactSchemas = {
   addSchema,
   updateFavoriteSchema,
 };
 module.exports = {
   Contact,
-  schemas,
+  contactSchemas,
 };
