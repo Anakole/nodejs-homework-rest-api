d05837ce47325b7c2572b3ff6dcec2e9da4c4606
Anakole <104356239+Anakole@users.noreply.github.com>
2023-03-22 23:19:09 +0200

    Update listContacts.js

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -1,8 +1,17 @@
 const { Contact } = require("../../models");
 
-const listContacts = async (_, res) => {
-  const result = await Contact.find();
-  res.json(result);
+const listContacts = async (req, res) => {
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 10, favorite } = req.query;
+  const skip = (page - 1) * limit;
+
+  const result = await Contact.find(
+    favorite ? { owner, favorite } : { owner },
+    "",
+    { skip, limit }
+  ).populate("owner", "_id email");
+
+  res.json({ code: 200, status: "Success", data: result });
 };
 
 module.exports = listContacts;
