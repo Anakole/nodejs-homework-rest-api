0910d89e15ed680eea5133893665ba36a74e2958
Anakole <104356239+Anakole@users.noreply.github.com>
2023-03-27 19:26:55 +0300

    Update login.js

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -18,6 +18,10 @@ const login = async (req, res) => {
     throw HttpError(401, "Email or password inwalid");
   }
 
+  if (!user.verify) {
+    throw HttpError(401, "Email not verify");
+  }
+
   const payload = {
     id: user._id,
   };
