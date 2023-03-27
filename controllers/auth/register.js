dac2ec776bfd8422daca4f2146b75b6c0f1ed29e
Anakole <104356239+Anakole@users.noreply.github.com>
2023-03-27 19:26:57 +0300

    Update register.js

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,7 +1,11 @@
 const bcrypt = require("bcrypt");
-const { HttpError } = require("../../helpers");
-const { User } = require("../../models");
 const gravatar = require("gravatar");
+const { v4 } = require("uuid");
+
+const { HttpError, sendEmail } = require("../../helpers");
+const { User } = require("../../models");
+
+const { BASE_URL } = process.env;
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -13,12 +17,22 @@ const register = async (req, res) => {
 
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationCode = v4();
 
   const newUser = await User.create({
     ...req.body,
     password: hashPassword,
     avatarURL,
+    verificationCode,
   });
+
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationCode}"> Click verify email </a>`,
+  };
+
+  await sendEmail(verifyEmail);
 
   res.status(201).json({
     code: 201,
